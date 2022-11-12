ced3e5027fa95e38413e3dc2b005998adb2ef0f3
SK <sarthakkarandikar03@gmail.com>
2022-11-12 05:53:45 +0000

    Nov 12, 2022, 11:23 AM

--- static/js/index.js
+++ static/js/index.js
@@ -7,6 +7,7 @@ var navItems = Array.from(document.querySelectorAll('.navbar-item a'))
 function myFunction() {
     if (window.pageYOffset >= sticky) {
         navbar.classList.add("sticky");
+        navItems.forEach()
     } else {
         navbar.classList.remove("sticky");
     }
