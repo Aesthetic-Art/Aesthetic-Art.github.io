09af584fee66552fb645183e9301a28e57196ce9
SK <sarthakkarandikar03@gmail.com>
2022-11-12 03:32:24 +0000

    Nov 12, 2022, 9:02 AM

--- static/js/index.js
+++ static/js/index.js
@@ -10,7 +10,7 @@ function showNextReview() {
     reviewName.innerHTML = "Sarthak"
     reviewImg.src = "./static/media/sarthak.svg"
     reviewTxt.innerHTML = "Hola"
-    
+    for (let i=)
 }
 
 function showPreviousReview() {
