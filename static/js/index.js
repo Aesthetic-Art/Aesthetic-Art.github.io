1d84b7ccab9a7d6207a3d56d7376da158f4803ff
SK <sarthakkarandikar03@gmail.com>
2022-11-12 05:52:30 +0000

    Nov 12, 2022, 11:22 AM

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ window.onscroll = function() {myFunction()};
 
 var navbar = document.getElementById("navbar");
 var sticky = navbar.offsetTop;
+var navItems = document.querySelectorAll('.')
 
 function myFunction() {
     if (window.pageYOffset >= sticky) {
