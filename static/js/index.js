e8e0f916beb9284490834d71dbecab29a725cabe
SK <sarthakkarandikar03@gmail.com>
2022-11-12 04:37:24 +0000

    Nov 12, 2022, 10:07 AM

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,7 @@ const reviews = [
         text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin sit amet neque orci. Orci varius natoque penatibus et magnis vulputate elementum non a orci"
     },
     {
-        image
+        image: "./static/media/"
     }
 ]
 
