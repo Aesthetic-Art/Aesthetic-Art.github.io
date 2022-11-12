7fe0214e57e58041cb73a14dab9a1a995510371e
SK <sarthakkarandikar03@gmail.com>
2022-11-12 03:20:53 +0000

    Nov 12, 2022, 8:50 AM

--- static/js/index.js
+++ static/js/index.js
@@ -8,7 +8,7 @@ let reviewCounter = 1
 
 function showNextReview() {
     reviewName.innerHTML = "Sarthak"
-    reviewImg.src=
+    reviewImg.src='./s'
 }
 
 nextReview.addEventListener('click', showNextReview, false)
