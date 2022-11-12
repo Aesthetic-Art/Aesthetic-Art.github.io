68fb20663cf172844dd094a08808a3c98e80ca2e
SK <sarthakkarandikar03@gmail.com>
2022-11-12 03:13:22 +0000

    Nov 12, 2022, 8:43 AM

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 var nextReview = document.querySelector('.review-next')
-var previousPreview=document.querySelector('.review-')
+var previousPreview=document.querySelector('.review-previous')
 var reviewDots = document.querySelectorAll('.review-dot')
 var reviewName = document.querySelector('.review-name')
 var reviewImg = document.querySelector('.review-img')
