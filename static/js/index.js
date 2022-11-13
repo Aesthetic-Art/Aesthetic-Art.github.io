9005ae7f4884a2724bd3a92a9ba204237c04a393
SK <sarthakkarandikar03@gmail.com>
2022-11-13 05:04:16 +0000

    Nov 13, 2022, 10:34 AM

--- static/js/index.js
+++ static/js/index.js
@@ -135,11 +135,11 @@ function showPreviousReview() {
     }
 
     else {
-        if (reviewCounter == 2) {
+        if (reviewCounter == 1) {
             previousReview.firstElementChild.style.color = "#7D5A50"
         }
     
-        else if (reviewCounter == 5) {
+        else if (reviewCounter == ) {
             nextReview.firstElementChild.style.color="white"
         }
     }
