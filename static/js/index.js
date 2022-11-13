9821628847fba3e6ab6d18c4a7574a94e043205b
SK <sarthakkarandikar03@gmail.com>
2022-11-13 04:45:07 +0000

    Nov 13, 2022, 10:15 AM

--- static/js/index.js
+++ static/js/index.js
@@ -103,7 +103,7 @@ function showPreviousReview() {
     reviewImg.src = reviews[reviewCounter-2]['image']
     reviewTxt.innerHTML = reviews[reviewCounter - 2]['text']
     
-    const b
+    const bigScree
 
     if (reviewCounter == 2) {
         previousReview.firstElementChild.style.color = "#7D5A50"
