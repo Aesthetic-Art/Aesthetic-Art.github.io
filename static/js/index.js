fc75cecadfb8e3296fa4781547b913d4b53a136a
SK <sarthakkarandikar03@gmail.com>
2022-11-16 14:36:19 +0000

    Nov 16, 2022, 8:06 PM

--- static/js/index.js
+++ static/js/index.js
@@ -37,7 +37,7 @@ const reviews = [
     {
         image: "./static/media/nidhi.svg",
         name: "Nidhi Pandhare",
-        text: "I bought a vintage transparent frame and genuinely loved it. Aesthetic Art dolor sit amet, consectetur adipiscing elit. Proin sit amet neque orci. Orci varius natoque penatibus et magnis vulputate elementum non a orci"
+        text: "I bought a vintage transparent frame and genuinely loved it. Aesthetic Art takes utmost care in serdolor sit amet, consectetur adipiscing elit. Proin sit amet neque orci. Orci varius natoque penatibus et magnis vulputate elementum non a orci"
     },
     {
         image: "./static/media/s.svg",
